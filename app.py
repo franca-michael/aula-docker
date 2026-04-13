f130480b8a8ace094f1ca78293c1c7486529f1a9
Michael <michael.mjpr@gmail.com>
2026-04-12 22:37:29 -0300

    config inicial para teste de docker

---
+++ app.py
@@ -0,0 +1,14 @@
+import streamlit as st
+
+def hello_world():
+    return "Hello, World! Welcome to Streamlit."
+
+def main():
+    st.title(hello_world())
+
+
+if __name__ == "__main__":
+    main()
+
+
+    
